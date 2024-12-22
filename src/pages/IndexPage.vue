57123a20751d92b4e9c5211bf3c22158de613aa1
Louis Chen <louischen2021uchi@gmail.com>
2024-12-22 14:36:43 +0800

    arrange layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,21 +1,22 @@
 <template>
-  <q-page class="flex flex-center">
-    <!-- <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    /> -->
-    <div>
+  <q-page class="flex flex-center" style="margin-top: 50px">
+    <div style="width: 400px">
       <q-input filled v-model="key" label="輸入加密金鑰" class="q-mb-md" />
-      <button id="modify-btn" @click="handleEncryptDES">Encrypt DES</button>
-      <button id="modify-btn" @click="handleEncryptAES">Encrypt AES</button>
-      <br />
-      <button id="modify-btn" @click="handleDecryptDES">Decrypt DES</button>
-      <button id="modify-btn" @click="handleDecryptAES">Decrypt AES</button>
 
-      <div>
-        <h5>檔案目前內容:</h5>
-        <p>{{ encrypted }}</p>
+      <h5>加密</h5>
+      <div class="button-group">
+        <button class="action-button" @click="handleEncryptDES">加密 DES</button>
+        <button class="action-button" @click="handleEncryptAES">加密 AES</button>
+      </div>
+      <h5>解密</h5>
+      <div class="button-group">
+        <button class="action-button" @click="handleDecryptDES">解密 DES</button>
+        <button class="action-button" @click="handleDecryptAES">解密 AES</button>
+      </div>
+
+      <div class="result-section">
+        <h5>檔案目前內容：</h5>
+        <p class="result-box">{{ encrypted }}</p>
       </div>
     </div>
   </q-page>
@@ -154,5 +155,42 @@ button {
 }
 button:hover {
   background-color: #358a6e;
+}
+
+h5 {
+  margin: 5px; /* 調整按鈕上下間距減小 */
+}
+
+.button-group {
+  display: flex;
+  justify-content: space-between; /* 分散按鈕 */
+  gap: 20px; /* 按鈕間距 */
+  flex-wrap: wrap; /* 當按鈕寬度超出父容器時換行 */
+}
+
+.action-button {
+  flex: 1 1 auto; /* 使按鈕等比例分布 */
+  text-align: center;
+  margin: 10px 0;
+  padding: 10px 20px;
+  background-color: #42b983;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  transition: background-color 0.3s;
+}
+
+.action-button:hover {
+  background-color: #358a6e;
+}
+.result-box {
+  background-color: #f0f0f0;
+  height: 300px;
+  padding: 10px;
+  border-radius: 5px;
+  word-wrap: break-word;
+  overflow: auto;
 }
 </style>
